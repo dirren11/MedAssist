9b0778b685ba98cd8ee7c2ad19e39e304ba478d6
dirren11 <dirrenbob@gmail.com>
2024-05-03 12:07:59 +0300

    Initial commit

---
+++ doctor.js
@@ -0,0 +1,169 @@
+export default doctorData = [
+  // Dentists: Kericho
+  {
+    need: 'Tooth Problem',
+    name: 'Tooth, Teeth, Dental, Dentist, Cavity, Toothache',
+    info: 'Pearl White Dental Care',
+    post: 'Dentist',
+    contact: '0740 688550',
+  },
+  {
+    need: 'Tooth Problem',
+    name: 'Tooth, Teeth, Dental, Dentist, Cavity, Toothache',
+    info: 'ELEPHANT DENTAL CLINIC KERICHO',
+    post: 'Dentist',
+    contact: ' 0718 305612',
+  },
+  {
+    need: 'Tooth Problem',
+    name: 'Tooth, Teeth, Dental, Dentist, Cavity, Toothache',
+    info: 'Ojwando, Dr Paul Odhiambo',
+    post: 'Dentist',
+    contact: ' 0722 785689',
+  },
+
+  {
+    need: 'Tooth Problem',
+    name: 'Tooth, Teeth, Dental, Dentist, Cavity, Toothache',
+    info: 'Dr.(Mrs) N.A. Nyanji',
+    post: 'Dentist',
+    contact: ' 0718 305612',
+  },
+  {
+    need: 'Tooth Problem',
+    name: 'Tooth, Teeth, Dental, Dentist, Cavity, Toothache',
+    info: 'Dr Betty langat',
+    post: 'Dentist',
+    contact: ' 0718 305612',
+  },
+
+  // Physiotherapy
+  {
+    need: 'Physiotherapy',
+    name: 'BackPain, NeckPain, BodyPain, Physiotherapy',
+    info: 'Physiosense Rehab',
+    post: 'Physiotherapist',
+    contact: '0777 007699',
+  },
+  {
+    need: 'Physiotherapy',
+    name: 'BackPain, NeckPain, BodyPain, Physiotherapy',
+    info: 'Fig Tree Health Options',
+    post: 'Physiotherapist',
+    contact: ' 0733 368358',
+  },
+
+
+  // Eye Clinics
+  {
+    need: 'Eye Problem',
+    name: 'Eye, Eyes, Optometry, Glasses, Eye Problem, Spectacles',
+    info: 'Jeska Eye & Medical Clinic',
+    post: 'Optometrist',
+    contact: '',
+  },
+  {
+    need: 'Eye Problem',
+    name: 'Eye, Eyes, Optometry, Glasses, Eye Problem, Spectacles',
+    info: 'Optica - Opticians in Kericho, Simbagate Mall',
+    post: 'Optometrist',
+    contact: '0709 709000',
+  },
+  {
+    need: 'Eye Problem',
+    name: 'Eye, Eyes, Optometry, Glasses, Eye Problem, Spectacles',
+    info: 'Afya Point Medicare Ltd',
+    post: 'Optometrist',
+    contact: ' 0722 637551',
+  },
+  {
+    need: 'Eye Problem',
+    name: 'Eye, Eyes, Optometry, Glasses, Eye Problem, Spectacles',
+    info: 'Macho Optical Ltd',
+    post: 'Optometrist',
+    contact: ' 0729 222700',
+  },
+  {
+    need: 'Eye Problem',
+    name: 'Eye, Eyes, Optometry, Glasses, Eye Problem, Spectacles',
+    info: 'Kericho Medical Centre',
+    post: 'Optometrist',
+    contact: 'Unavailable',
+  },
+  {
+    need: 'Eye Problem',
+    name: 'Eye, Eyes, Optometry, Glasses, Eye Problem, Spectacles',
+    info: 'FigTree Health Options Main Branch',
+    post: 'Optometrist',
+    contact: ' 0733 368358',
+  },
+
+
+  // Heart Problems
+  {
+    need: 'Heart Problem',
+    name: 'Heart, Cardiologist',
+    info: 'Kericho County Referral Hospital',
+    post: 'Cardiologist',
+    contact: '0758 721987',
+  },
+  {
+    need: 'Heart Problem',
+    name: 'Heart, Cardiologist',
+    info: 'SILOAM HOSPITAL',
+    post: 'Cardiologist',
+    contact: '0728 881121',
+  },
+  {
+    need: 'Heart Problem',
+    name: 'Heart, Cardiologist',
+    info: 'Aga khan hospital kericho',
+    post: 'Cardiologist',
+    contact: '9801872311',
+  },
+  {
+    need: 'Heart Problem',
+    name: 'Heart, Cardiologist',
+    info: 'Kericho Outpatient Medical Centre',
+    post: 'Fig Tree Health Options',
+    contact: '0700 695464',
+  },
+  {
+    need: 'Heart Problem',
+    name: 'Heart, Cardiologist',
+    info: 'Greenview Nursing Home',
+    post: 'Registrar Cardiology',
+    contact: '0745 315600',
+  },
+  {
+    need: 'Heart Problem',
+    name: 'Heart, Cardiologist',
+    info: 'Dr. Bijay Khadka',
+    post: 'Oracle Healthcare Ltd',
+    contact: '0745 942180',
+  },
+
+
+  // Skin
+  {
+    need: 'Skin Problem',
+    name: 'SkinCare, Skin',
+    info: 'Kericho Womens Clinic',
+    post: 'Dermatologist',
+    contact: '0790 816200',
+  },
+  {
+    need: 'Skin Problem',
+    name: 'SkinCare, Skin',
+    info: 'Kericho Medical Clinic',
+    post: 'Dermatologist',
+    contact: '0729 945105',
+  },
+  {
+    need: 'Skin Problem',
+    name: 'SkinCare, Skin',
+    info: 'Kericho Outpatient Medical Centre',
+    post: 'Dermatologist',
+    contact: '0700 695464',
+  },
+];
